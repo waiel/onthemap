be9d84b6af9e8ede19f3bc67e3ef1db63dce5e42
Waiel Eid <waiel@waieleid.com>
2019-05-06 10:26:49 +0300

    Initial Commit

---
+++ onTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onTheMap
+//
+//  Created by Waiel Eid on 6/5/19.
+//  Copyright © 2019 Waiel Eid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
