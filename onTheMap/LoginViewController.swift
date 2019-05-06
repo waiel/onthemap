d8de9d33df492842fddc19f1332e1797a494d3cf
Waiel Eid <waiel@waieleid.com>
2019-05-06 14:49:41 +0300

    login controller code alert for empty fields

--- onTheMap/LoginViewController.swift
+++ onTheMap/LoginViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.isHidden = true
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        
+        activityIndicator.startAnimating();
+        
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        if( email.isEmpty || password.isEmpty) {
+            let alert = UIAlertController(title: "Information required", message: "Email and Password can not be empty!\n Please fill the required fileds to login", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil) )
+            
+            self.present(alert, animated: true)
+        }
+        
+        
+        activityIndicator.stopAnimating()
+        
+    }
+    
 }
 
